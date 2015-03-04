111151d523d7ecc4155af226e1727aa8ce5e8abe
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2015-03-04 22:04:46 +0100

    IPython notebook containing the code examples seen in the presentation

--- 11_svm.py
+++ 11_svm.py
@@ -11,5 +11,5 @@ clf = svm.SVC(kernel='rbf')
 clf.fit(X, y)
 
 # Doing predictions
-new_data = [[4.85, 3.1], [5.61, 3.02], [6.63,  3.13]]
+new_data = [[4.85, 3.1], [5.61, 3.02]]
 print clf.predict(new_data)
